bfd5684cad4b47e8f0416fa4587405761037f5ca
drkup <darrenkwondev46@gmail.com>
2025-02-23 03:58:44 +0900

    feat: neotree

--- lua/plugins/sidebar.lua
+++ lua/plugins/sidebar.lua
@@ -1,14 +1,40 @@
 return {
   {
-    "folke/edgy.nvim",
-    event = "VeryLazy",
-    init = function()
-      -- 전역 상태줄을 하나로 설정 (전체 상태줄)
-      vim.opt.laststatus = 3
-      -- 창 분할 시 메인 에디터의 레이아웃 이동을 방지
-      vim.opt.splitkeep = "screen"
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",        -- 필수
+      "nvim-tree/nvim-web-devicons",  -- 아이콘 (권장)
+      "MunifTanjim/nui.nvim",         -- UI 라이브러리
+    },
+    -- Ctrl + b로 토글할 수 있도록 키 매핑
+    keys = {
+      {
+        "<C-b>",
+        function()
+          require("neo-tree.command").execute({
+            source = "filesystem",  -- 파일 트리 소스
+            position = "left",      -- 왼쪽에 표시
+            toggle = true,          -- 토글(열려 있으면 닫고, 닫혀 있으면 열기)
+          })
+        end,
+        desc = "Toggle Neo-tree filesystem",
+      },
+    },
+    config = function()
+      require("neo-tree").setup({
+        -- 최후의 창이 neo-tree라면 자동 닫기
+        close_if_last_window = true,
+        -- 파일 시스템 소스 관련 최소 설정
+        filesystem = {
+          follow_current_file = true,       -- 현재 열려 있는 파일 위치 자동 포커스
+          use_libuv_file_watcher = true,    -- 실시간으로 파일 변화 감지
+        },
+        window = {
+          width = 30, -- 사이드바 너비
+        },
+      })
     end,
-    opts = {}  -- 기본 옵션 그대로 사용
-  }
+  },
 }
 
