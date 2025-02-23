ff4a9b487ec45d090bf46bd3fb050707f08089e4
drkup <darrenkwondev46@gmail.com>
2025-02-23 14:06:35 +0900

    feat: ctags keymap

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -4,3 +4,6 @@ vim.keymap.set("n", "<D-s>", "<cmd>w<CR>", { silent = false })
 -- 전체 코드 자동 들여쓰기 후 커서 복귀(lsp, formatter 안 쓰고 단순 indent)
 vim.keymap.set("n", "<Leader>g", "mzgg=G`z", { noremap = true, desc = "전체 코드 자동 들여쓰기 후 커서 복귀" })
 
+-- ctag 생성
+vim.keymap.set('n', '<f5>', ':!ctags -R --verbose<CR>', { desc = 'Run ctags on the entire project' })
+
